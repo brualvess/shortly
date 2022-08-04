46feee1fbfaa9cc8abf3b3fd320ca004480ac2fa
brualvess <oliveirabrunna2210@gmail.com>
2022-08-04 12:11:28 -0300

    signin

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,6 +1,7 @@
 import { connection } from '../dbStrategy/postgres.js'
 import joi from 'joi'
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 export async function createUsers(req, res) {
     const datas = req.body
     const schemaUser = joi.object({
@@ -37,4 +38,32 @@ export async function createUsers(req, res) {
     }
     }
 
+}
+export async function loginUsers(req, res){
+    const datas = req.body
+    const token = uuid();
+    const schemaUser = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+    })
+    const { error } = schemaUser.validate(datas)
+    if (error) {
+        res.sendStatus(422)
+        return
+    }
+    const {rows:user} = await connection.query(
+        `SELECT * FROM users WHERE email = '${datas.email}'`
+        )
+        if(!user[0]){
+            res.sendStatus(401)
+            return
+        }
+    const verificarSenha = bcrypt.compareSync(
+        datas.password, user[0].password)
+
+        if( !verificarSenha){
+            res.sendStatus(401)
+            return
+        } 
+      res.status(200).send(token) 
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { createUsers } from './controllers/usersControllers.js';
+import { createUsers,
+         loginUsers
+} from './controllers/usersControllers.js';
 
 const router = Router()
 
 // users routes
 router.post('/signup', createUsers)
+router.post('/signin',loginUsers )
 
 
 export default router
