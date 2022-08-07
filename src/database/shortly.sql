6c0d3370d4f664bb77308861ccbd619ed9d8a3cf
brualvess <oliveirabrunna2210@gmail.com>
2022-08-07 15:59:29 -0300

    fix

--- src/database/shortly.sql
+++ src/database/shortly.sql
@@ -3,24 +3,32 @@ CREATE TABLE users (
 	"name" varchar(110) NOT NULL,
 	"email" TEXT NOT NULL UNIQUE,
 	"password" TEXT NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL,
 	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 );
 
-CREATE TABLE tokens(
+
+CREATE TABLE tokens (
 	"id" serial NOT NULL,
 	"token" TEXT NOT NULL,
 	"userId" integer NOT NULL,
 	"expireAt" TIMESTAMP NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL,
 	CONSTRAINT "tokens_pk" PRIMARY KEY ("id")
 );
+
+
 
 CREATE TABLE "shortUrl" (
 	"id" serial NOT NULL,
 	"url" TEXT NOT NULL,
 	"key" TEXT NOT NULL,
 	"userId" bigint NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL,
 	CONSTRAINT "shortUrl_pk" PRIMARY KEY ("id")
 );
+
+
 
 CREATE TABLE visits (
 	"id" serial NOT NULL,
@@ -30,21 +38,13 @@ CREATE TABLE visits (
 );
 
 
+
+
 ALTER TABLE "tokens" ADD CONSTRAINT "tokens_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+
 ALTER TABLE "shortUrl" ADD CONSTRAINT "shortUrl_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+
 ALTER TABLE "visits" ADD CONSTRAINT "visits_fk0" FOREIGN KEY ("urlId") REFERENCES "shortUrl"("id");
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
